9a6690e8ec2e5402b419af69f7254385d1867a4d
Edward Angelo Salas <zyro670@gmail.com>
2022-05-09 07:26:18 -0400

    commit tree 323cc5e38fe41c5d05a6a14755cdd402b2a44f64

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your code here
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
+  
+  // method: "POST" is missing from the object below
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+  fetch("http://localhost:3000/dogs", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    })
+    .catch(function (error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+    });
+
+    function submitData(name, email) {
+      fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: (JSON.stringify(name, email)),
+      })
+    }
